938cd7d41dc59df3f616f28167278ef1ddef065e
Shreejai Raj <shreejairaj@gmail.com>
2024-06-01 12:06:20 +1000

    color fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
       </div>
     </div>
 
-    <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+    <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2] flex items-center justify-center absolute top-0 left-0">
       
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
       
@@ -31,7 +31,7 @@ const Hero = () => {
         words='Transforming Concepts into Seamless Experiences'
         />
 
-        <p className='text-center md:tracking-wider text-sm md:text-lg lg:text-2xl'>
+        <p className='text-center md:tracking-wider text-sm md:text-lg lg:text-2xl mb-4'>
           Hi, I&apos;m Shreejai, a Next.js Developer based in Sydney, Australia.
         </p>
 
