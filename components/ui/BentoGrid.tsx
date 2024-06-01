302741840500134f3ffa7ae3fb5bac265760eede
Shreejai Raj <shreejairaj@gmail.com>
2024-06-01 16:24:47 +1000

    globe 3d

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -45,7 +45,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 ",
+        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 ",
         className
       )}
       style={{
@@ -59,9 +59,9 @@ export const BentoGridItem = ({
         <div className="w-full h-full absolute">
           {img && (
             <img
-            src={img}
-            alt={img}
-            className={cn(imgClassName, 'object-cover', 'object-center')}
+              src={img}
+              alt={img}
+              className={cn(imgClassName, "object-cover object-center ")}
             />
           )}
         </div>
